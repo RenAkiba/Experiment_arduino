a2b525a1998e77ff3375d05b93fca33eede2c5ba
RenAkiba <phasu.ner.61.19@gmail.com>
2023-06-22 11:20:30 +0900

    second commit

--- experiment.ino
+++ experiment.ino
@@ -3,18 +3,19 @@
   論理積をとる
   11111 = 1, 異常なし
   11101 = 0, 異常あり 
+
+  IRセンサの真理値
+  接触時 0
+  非接触時 1
+  なので以下では反転させる
 */
 
-
+/*--------------------------include, 変数宣言-----------------------------------*/
 #include <avr/sleep.h>
 
-void WakeUp(){
-  delay(1);
-}
-
 //IRセンサの入力
-const int IR1 = 1;
-const int IR2 = 2;//真ん中
+const int IR1 = 4;
+const int IR2 = 2; //真ん中のセンサ, 割り込み処理を行うために2番ピン
 const int IR3 = 3;
 
 //Moisture senser
@@ -24,14 +25,12 @@ const int MS3 = A3;
 
 const int SPEAKER = 12;
 
-int set_time;
+int sleep_count = 0;
 
 #define BEATTIME 200 //音を出している時間(msec)
-#define n 450
+#define S 50
 
-void SetTime(){
-  set_time = millis();
-}
+/*---------------------------------------------------------------------*/
 
 void setup(){
   Serial.begin(9600);
@@ -39,41 +38,40 @@ void setup(){
   pinMode(IR2, INPUT);
   pinMode(IR3, INPUT);
   pinMode(SPEAKER, OUTPUT);
-
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  attachInterrupt(IR2, WakeUp, FALLING);
-  attachInterrupt(IR2, SetTime, RISING);
+  attachInterrupt(digitalPinToInterrupt(IR2), wakeup, CHANGE);
 }
 
 void loop(){
 
-  int IRin_1 = digitalRead(IR1);
-  int IRin_2 = digitalRead(IR2);
-  int IRin_3 = digitalRead(IR3);
+  int IRin_1 = !(digitalRead(IR1));
+  int IRin_2 = !(digitalRead(IR2));
+  int IRin_3 = !(digitalRead(IR3));
   int MSin_1, MSin_2, MSin_3; //水分センサの入力値
   int Logic1, Logic2, Logic3; //水分センサの入力値で変化する論理
 
-  //1番のセンサ////////////////////////////
+  /*---------------------------1番センサ-------------------------------*/
   if(IRin_1 == 1){
     MSin_1 = analogRead(MS1);//値をとる
 
-    if(MSin_1 > n){//閾値以上なら
+    if(MSin_1 > 470){//閾値以上なら
       Logic1 = 1;//問題なし
     }else{
       Logic1 = 0;//以下なら問題あり
     }
 
   }else{
-    MSin_1 = -1;//値をとらない
-    Logic1 = 1;//問題なし
+    MSin_1 = -1;//エラー値とする。センサーの値はとらない
+    Logic1 = 1;//スピーカーを鳴らすif文に干渉しないように,問題なし1
   }
+  /*---------------------------------------------------------------------*/
   
 
-  //2番のセンサ////////////////////////////
+  /*----------------------------2番センサ------------------------------*/
   if(IRin_2 == 1){
     MSin_2 = analogRead(MS2);
 
-    if(MSin_2 > n){
+    if(MSin_2 > 470){
       Logic2 = 1;
     }
     else{
@@ -84,14 +82,15 @@ void loop(){
     MSin_2 = -1;
     Logic2 = 1;
   }
+  /*---------------------------------------------------------------------*/
   
 
 
-  //3番のセンサ////////////////////////////
+  /*----------------------------3番センサ-------------------------------*/
   if(IRin_3 == 1){
     MSin_3 = analogRead(MS3);
 
-    if(MSin_3 > n){
+    if(MSin_3 > 480){
       Logic3 = 1;
     }
     else{
@@ -102,28 +101,33 @@ void loop(){
     MSin_3 = -1;
     Logic3 = 1;
   }
+  /*---------------------------------------------------------------------*/
 
 
-  if((Logic1 && Logic2) && Logic3 == 0){ //Logicの論理積が0なら(湿潤時なら)
-    tone(SPEAKER,440,BEATTIME) ; // ラ
-    delay(BEATTIME) ;
+  if((Logic1 && Logic2 && Logic3) != 1){ //Logicの論理積が0なら(湿潤時なら)
+    tone(SPEAKER,440,BEATTIME);
+    delay(BEATTIME);
+    sleep_count++;
+
+    if(sleep_count > S){
+      sleep_enable();
+      sleep_cpu();
+      sleep_disable();
+      sleep_count = 0;
+    }
   }
 
-  
-  int now_time = millis();
-  if(set_time - now_time == 3*60*1000){ //millis()は50日でオーバーフロー。絶対値をとることでset_time - now_timeが負の値になることを防ぐ
-    //sleepモードと割り込みを有効化
-    sleep_enable();
+  sleep_count++;
 
-    //割り込みの条件を設定、真ん中のIR2の立下り(タオルを持ち上げたとき)
-    sleep_cpu();
-
-    //sleepモードと割り込みを無効化
-    sleep_disable();
+  if(sleep_count > S){
+      sleep_enable();
+      sleep_cpu();
+      sleep_disable();
+      sleep_count = 0;
   }
-  
+   
 
-
+/*-----------------------------デバッグ用----------------------------------
   Serial.print("IR1:");
   Serial.println(IRin_1);
 
@@ -150,15 +154,13 @@ void loop(){
 
   Serial.print("Logic3:");
   Serial.println(Logic3);
+
   Serial.println("--------------------------------");
+  */
 }
 
 
-void SetTime(){
-  set_time = millis();
-}
-
-void WakeUp(){
+void wakeup(){
   ;
 }
 
